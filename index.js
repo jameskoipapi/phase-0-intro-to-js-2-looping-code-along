36bc804da513d1e61a44b17dae7e9573b073d776
Moringa School <james.koipapi@student.moringaschool.com>
2022-09-29 09:34:01 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// const names = ["Guadaleupe", "Ollie", "Aki"];
+function writeCards(names, event) {
+    let newArry = [];
+    for (let i = 0; i < names.length; i++) {
+        newArry.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+
+  }
+  return newArry;
+}
+
+function countDown () {
+    let i=10;
+    while (i > -1) {
+        console.log(i--);
+
+    }
+    return i;
+}
